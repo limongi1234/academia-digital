bea252c1e41364850f5783f358ada1f14f7a4415
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-08-02 20:41:15 -0300

    Update AcademiaDigitalApplication.java

--- src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
+++ src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademiaDigitalApplication {
+public class AcademiaDigitalApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(AcademiaDigitalApplication.class, args);
 	}
 
