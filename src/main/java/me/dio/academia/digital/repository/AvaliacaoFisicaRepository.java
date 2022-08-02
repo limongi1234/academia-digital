4516de1870fdc75bd913058d5f66b9545f86eb31
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-08-02 20:28:14 -0300

    Update AvaliacaoFisicaRepository.java

--- src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> 
+{
 
 }
